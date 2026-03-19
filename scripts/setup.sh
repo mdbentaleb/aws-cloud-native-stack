3f32e77397aaba68f72a61a0b9ad66a1067a547f
Mohamed Bentaleb <bent4leb@gmail.com>
2026-03-19 01:43:47 +0000

    Fix AWS & Docker installation script error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y awscli docker.io docker-compose
+sudo apt update -y && sudo apt upgrade -y
 
+sudo apt install -y docker.io unzip curl
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -o awscliv2.zip
+sudo ./aws/install --update
+rm -rf awscliv2.zip aws/
+
+if ! getent group docker > /dev/null; then
+    sudo groupadd docker
+fi
 sudo usermod -aG docker $USER
 
-mkdir -p nginx app
+/usr/local/bin/aws s3 cp s3://my-app-nginx-config/default.conf ./nginx/default.conf
 
-aws s3 cp s3://my-app-nginx-config/default.conf ./nginx/default.conf
-
-sudo docker-compose up --build -d
+sudo /usr/local/bin/docker-compose up --build -d
